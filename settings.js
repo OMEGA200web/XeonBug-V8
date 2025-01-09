4a2772341540bcbf5c227f6f1710985e44676465
OMEGA200web <davidchisom200@gmail.com>
2025-01-09 16:51:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,13 +11,13 @@ const chalk = require('chalk')
 
 //contact details
 global.ownernomer = "2349051000905"
-global.ownername = "Omegæ"
+global.ownername = "Toxxic"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
 global.ownernumber = '2349051000905'  //creator number
-global.ownername = 'Omegæ' //owner name
+global.ownername = 'Toxxic' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
